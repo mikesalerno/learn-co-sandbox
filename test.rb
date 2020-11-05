77e09893ef93351fe896a241edbfafcbd265a1ad
Michael <wtfdic@hotmail.com>
2020-11-05 03:44:52 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,39 @@
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+puts stylish_chef
+
+def change_my_name
+  name = "Frank"
+  puts name.reverse
+  return name
+end
+
+puts change_my_name
+
+puts 6 + 3
+
+total = 6 + 3
+
+puts total
+
+new_total = total + 10 
+
+puts new_total
+
+def print_one_plus_one
+  puts 1+1
+end
+
+print_one_plus_one
+
+def one_plus_one 
+  1+1 
+end
+
+puts one_plus_one
+
+puts total + 17
